221ef24e5543862adff5afa293af2d7e85f8f75f
William Chiu <william.chiu16@trinityschoolnyc.org>
2015-06-27 10:19:24 -0700

    Update ripmanga.js

--- ripmanga.js
+++ ripmanga.js
@@ -11,33 +11,36 @@ $.YQL = function(query, callback) {
 	url = 'http://query.yahooapis.com/v1/public/yql?q='
 	+ encodedQuery + '&format=json&callback=?';
 
-	$.getJSON(url, callback);
+	$.ajax({
+		url: url,
+		dataType: 'json',
+		async: false,
+		success: callback
+	});
 
 };
 
-function setup() { 
-	//http://james.padolsey.com/snippets/using-yql-with-jsonp/
-	$.YQL("select * from html where url='" + MANGAHERE_URL + "'", function(data) {
-		mangapage = document.createElement('p');
 
-		mangapagelinksnumber = Object.keys(data.query.results.body.section.article.div.div[1].div[2].ul[0].li).length;
-		addlinks = "";
+$.holdReady( true );
 
-		for (temp = 0; temp < mangapagelinksnumber; temp++) {
-			addlinks += "<a href=" + JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.href) + ">" + JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.content).substring(19, JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.content).length - 13) + "</a></br>";
-			mangapagelinksarray.push(JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.href).substring(1, JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.href).length - 1));
-		}
+//http://james.padolsey.com/snippets/using-yql-with-jsonp/
+$.YQL("select * from html where url='" + MANGAHERE_URL + "'", function(data) {
+	mangapage = document.createElement('p');
 
-		mangapage.innerHTML = addlinks;
-		document.getElementsByTagName('body')[0].appendChild(mangapage);
+	mangapagelinksnumber = Object.keys(data.query.results.body.section.article.div.div[1].div[2].ul[0].li).length;
+	addlinks = "";
 
-		$.YQL("select * from html where url='" + mangapagelinksarray[mangapagelinksarray.length-1] + "'", function(data) {
-			console.log(mangapagelinksarray[mangapagelinksarray.length-2]);
-			console.log(JSON.stringify(data.query.results.body.section[1].a.img.src).substring(1, JSON.stringify(data.query.results.body.section[1].a.img.src).length - 1));
-			tempimg = createImg(JSON.stringify(data.query.results.body.section[1].a.img.src).substring(1, JSON.stringify(data.query.results.body.section[1].a.img.src).length - 1));
-			tempimg.hide();
-			createCanvas(tempimg.width, tempimg.height);
-			image(tempimg, 0, 0);
-		});
+	for (temp = 0; temp < mangapagelinksnumber; temp++) {
+		addlinks += "<a href=" + JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.href) + ">" + JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.content).substring(19, JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.content).length - 13) + "</a></br>";
+		mangapagelinksarray.push(JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.href).substring(1, JSON.stringify(data.query.results.body.section.article.div.div[1].div[2].ul[0].li[temp].span[0].a.href).length - 1));
+	}
+
+	mangapage.innerHTML = addlinks;
+	document.getElementsByTagName('body')[0].appendChild(mangapage);
+
+	$.YQL("select * from html where url='" + mangapagelinksarray[mangapagelinksarray.length-1] + "'", function(data) {
+		console.log(mangapagelinksarray[mangapagelinksarray.length-2]);
+		console.log(JSON.stringify(data.query.results.body.section[1].a.img.src).substring(1, JSON.stringify(data.query.results.body.section[1].a.img.src).length - 1));
+		$.holdReady( false );
 	});
-} 
+});
